c5329b19ff86a8508724c151bfba5e59172a2c63
raman-webdev <chaudharyraman562@gmail.com>
2025-01-09 22:10:37 +0545

    haha

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,46 @@ import React , {useState} from 'react'
 
 const App = () => {
   
-  let [counter, setCounter] = useState(0)
+  let [counter, setCounter] = useState(0);
+  const [message, setMessage] = useState('');
   // let counter = 0;
 
   const addValue = () => {
-    counter = counter + 1;
-    setCounter(counter)
-    console.log("Hello", counter)
+    if (counter < 20){
+      counter = counter + 1;
+      setCounter(counter);
+      setMessage('');
+    }
+    else{
+      setMessage('You reached maximum value i.e. 20!!')
+    }
   }
 
   const subtractValue = () => {
-    counter = counter - 1;
-    setCounter(counter)
+    if(counter > 0){
+      setCounter(counter - 1);
+      setMessage('');
+    }
+    else{
+      setMessage('You reached minimum value i.e. 20!!');
+    }
+    
   }
 
   const reset = () => {
     counter = 0;
-    setCounter(counter)
+    setCounter(counter);
+    setMessage('You just clicked Reset! Tap Increment Button!!');
   }
 
   return (
     <>
     <div className='w-full h-screen bg-slate-600 flex flex-col justify-center items-center'>
       <h1 className='text-green-400 font-bold underline'>COUNTER APP</h1>
+
+      {message && ( <p className='mt-2 text-rose-400 font-semibold'>
+        {message}
+      </p> )}
 
       <h2 className='font-semibold mt-2 hover:font-bold'>Value: {counter}</h2>
 
